b9147f817fe69bd6aa321f1ddbfd5be4aaaf94e1
Kent Anderson <kntndrsn@gmail.com>
2020-01-27 21:44:22 -0600

    Finished section 9

--- clientms/clients/admin.py
+++ clientms/clients/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ClientAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Comment)
